3a5c64d6c704338375f703a11f4e33e7b706d2e0
eddpool97 <edwin.e.ayala1997@gmail.com>
2024-08-29 17:48:09 -0600

    Aplicacion de listener y 'Clean Up' de listener

--- src/02-useEffect/Message.jsx
+++ src/02-useEffect/Message.jsx
@@ -1,20 +1,30 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 
 export const Message = () => {
 
+    const [ coords, setCoords] = useState({x: 0, y:0});
+
     useEffect(() => {
-      console.log('Message Mounted');
+
+        const onMouseMove = ({x, y})=>{
+            setCoords({ x, y })
+        }
+      
+        window.addEventListener( 'mousemove', onMouseMove);
     
-      return () => {
-        console.log('Message Unmounted');
-      }
-    }, [])
+        //CleanUp de Listener y Observables
+        return () => {
+            window.removeEventListener('mousemove', onMouseMove);
+        }
+
+    }, []);
     
 
     return (
         <>
             <h3>Usuario ya existe</h3>
+            { JSON.stringify( coords ) }
         </>
     )
 }
