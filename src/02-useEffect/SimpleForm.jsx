6de237435cdaaa31734957c395f8662e242abad0
eddpool97 <edwin.e.ayala1997@gmail.com>
2024-08-29 16:46:06 -0600

    Profundizando Hooks. Estudios de 'useState' y 'useEffect'

---
+++ src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react';
+import { Message } from './Message';
+
+export const SimpleForm = () => {
+
+    const [formState, setFormState] = useState({
+        username: 'eddpool',
+        email: 'edwin.ayala@google.com'
+
+    });
+
+    const {username, email} = formState;
+
+    const onInputChange = ( { target } )=>{
+        const { name, value } = target;
+        setFormState({
+            ...formState,
+            //Propiedad computada de objetos en JavaScript
+            [ name ]: value
+        });
+    }
+
+    useEffect( ()=>{
+        //console.log('FormState changed');
+    }, [ formState ]);
+
+    useEffect( ()=>{
+        //console.log('email changed');
+    }, [ email ]);
+
+    return (
+        <>
+            <h1>Simple Form</h1>
+            <hr />
+
+            <input 
+                type="text" 
+                className="form-control mt-2"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={ onInputChange }
+            />
+
+            <input 
+                type="email" 
+                className="form-control mt-2"
+                placeholder="edwin@google.com"
+                name="email"
+                value={email}
+                onChange={ onInputChange }
+            />
+
+            {
+                //Condicional 'if' abreviada
+                ( username === 'eddpool97' ) && <Message />
+            }
+        </>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HooksApp } from './HooksApp'
-import './index.css'
+//import { HooksApp } from './HooksApp'
+//import { CounterApp } from './01-useState/CounterApp'
+//import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
+import './index.css';
+import { SimpleForm } from './02-useEffect/SimpleForm';
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <HooksApp />
-  </StrictMode>,
+  //<StrictMode>
+    <SimpleForm/>
+  //</StrictMode>,
 )
